60eb09a9fbd56061c8562cd32bc1aa925ab0f4bb
sacidaksoy <sacidofficial@gmail.com>
2022-07-04 21:41:05 +0300

    feat: add getInitialProps [fetch Data]

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,31 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
 
-export default () => (
-    <Layout title={'About'}>
-        <Link href="/">
-            <a>Go to home page</a>
-        </Link>
-        <p>A Javascript Programmer</p>
-        <img src="/static/js.jpg" alt="Javascript" height="200px" />
-    </Layout>
-)
+import React, { Component } from 'react'
+
+export class About extends Component {
+
+    static async getInitialProps() {
+        const res = await fetch('https://api.github.com/users/sacidaksoy');
+        const data = await res.json();
+        return { user: data };
+    }
+
+    render() {
+        const { user } = this.props;
+        return (
+            <Layout title={'About'}>
+                <Link href="/">
+                    <a>Go to home page</a>
+                </Link>
+                <p>{user.login}</p>
+                <img src={user.avatar_url} alt={user.login} height="200px" />
+                <Link href={user.html_url}>
+                    <a>{user.html_url}</a>
+                </Link>
+            </Layout>
+        )
+    }
+}
+
+export default About;
